6a50854d5eb087d8f7ec95aaeb3be3fcfbd2d17d
hyer0705 <hyer1205@gmail.com>
2020-02-19 23:59:49 +0900

    Express Core : Middlewares

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const handleHome = (req, res) => res.send('I\'m out of shape');
 
 const handleProfile = (req, res) => res.send('Welcome my Profile page!');
 
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
