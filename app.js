8066c885790064a88b516a37486cd6d8898aa2b2
hyer0705 <hyer1205@gmail.com>
2020-02-24 00:18:12 +0900

    Template Variables in Pug

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ import morgan from "morgan";
 import helmet from "helmet"; // 보안과 관련된 미들웨어
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
-import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug"); // view engine 을 pug로 바꿈
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
