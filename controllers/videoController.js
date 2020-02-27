11803a1f0380ec3b82f7c2180179052cfb5ea6c7
hyer0705 <hyer1205@gmail.com>
2020-02-27 15:33:54 +0900

    Join Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -7,7 +7,7 @@ export const home = (req, res) => {
 export const search = (req, res) => {
     // console.log(req.query);
     const { query: { term: searchingBy } } = req;
-    res.render("search", { pageTitle: "Search", searchingBy });
+    res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) => res.render("upload", { pageTitle: "Video Upload" });
