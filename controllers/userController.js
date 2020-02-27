f8e86d8df5c7224f6a369b036a38671ddf588994
hyer0705 <hyer1205@gmail.com>
2020-02-27 16:22:39 +0900

    Log In and User Profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,9 +23,13 @@ export const postJoin = (req, res) => {
     }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+};
+
 export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" });
-export const users = (req, res) => res.send("Users");
+// export const users = (req, res) => res.send("Users");
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
