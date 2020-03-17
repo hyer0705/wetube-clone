5ac7f20f19ec8250bfa4cd2401155eda5a16ecc3
hyer0705 <hyer1205@gmail.com>
2020-03-18 06:12:51 +0900

    Github Log In part Two

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
-import { getJoin, logout, postJoin, getLogin, postLogin } from "../controllers/userController";
-import { onlyPublic } from "../middlewares";
+import { getJoin, logout, postJoin, getLogin, postLogin, githubLogin, postGithubLogin } from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 
 const globalRouter = express.Router();
 
@@ -17,6 +18,14 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.logout, onlyPublic, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
+
+// github login
+globalRouter.get(routes.github, githubLogin);
+
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate("github", { failureRedirect: '/login' }), postGithubLogin
+);
 
 export default globalRouter;
